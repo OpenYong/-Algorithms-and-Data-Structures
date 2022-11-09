fcbd3beed602d015e01d758abba3495968bf6311
Yong <fkszm3@gmail.com>
2022-11-09 23:01:43 +0900

    Stacks - pop()

--- 21-stacks-cueues/Stack.js
+++ 21-stacks-cueues/Stack.js
@@ -25,6 +25,26 @@ class Stack {
     }
     return ++this.size;
   }
+
+  // shift처럼
+  pop() {
+    if (!this.first) return null;
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
 }
 
 let stack = new Stack();
+
+stack.push("111");
+stack.push("222");
+stack.push("333");
+stack.push("444");
+console.log(stack);
+stack.pop();
+console.log(stack);
