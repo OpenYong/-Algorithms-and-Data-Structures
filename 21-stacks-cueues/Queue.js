32c87dba879b1558089ad652a4230536cc700159
OpenYong <fkszm3@gmail.com>
2022-11-11 18:18:08 +0900

    Stacks and queues - Queues

---
+++ 21-stacks-cueues/Queue.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return this.size++;
+  }
+
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+
+    // 사이즈가 1일때
+    if (this.first === this.last) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.size--;
+
+    return this;
+  }
+}
+
+let myQueue = new Queue();
+myQueue.enqueue("첫번째");
+myQueue.enqueue("두번째");
+myQueue.enqueue("세번째");
+
+myQueue.dequeue();
+
+console.log(myQueue);
