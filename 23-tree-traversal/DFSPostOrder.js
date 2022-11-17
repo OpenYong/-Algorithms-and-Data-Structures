a41c113f7e5be8a2f0094e46e5a769dbf89890aa
OpenYong <fkszm3@gmail.com>
2022-11-17 12:07:31 +0900

    Tree Traversal - DFSPostOrder

---
+++ 23-tree-traversal/DFSPostOrder.js
@@ -0,0 +1,86 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.value) return undefined;
+      if (val > current.value) {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      } else if (val < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      }
+    }
+  }
+
+  find(val) {
+    if (!this.root) return;
+    let current = this.root;
+    while (true) {
+      if (val === current.value) return current;
+      if (val > current.value) {
+        if (!current.right) {
+          return undefined;
+        }
+        current = current.right;
+      } else if (val < current.value) {
+        if (!current.left) {
+          return undefined;
+        }
+        current = current.left;
+      }
+    }
+  }
+
+  // 깊이 우선 탐색 - 후위 순회
+  DFSPostOrder() {
+    let current = this.root;
+    let visited = [];
+
+    function traverse(current) {
+      if (current.left) traverse(current.left);
+      if (current.right) traverse(current.right);
+      visited.push(current.value);
+    }
+
+    traverse(current);
+    return visited;
+  }
+}
+
+var tree = new BinarySearchTree();
+
+tree.insert(51);
+tree.insert(23);
+tree.insert(12);
+tree.insert(85);
+tree.insert(75);
+tree.insert(76);
+tree.insert(99);
+tree.insert(100);
+tree.DFSPostOrder();
+
+console.log(tree.DFSPostOrder());
