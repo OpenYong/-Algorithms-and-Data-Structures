1ec30f3bb4836a99d2f15a6a864a2edfa5d3892a
OpenYong <fkszm3@gmail.com>
2023-01-10 09:37:38 +0900

    Graph - addEdge

---
+++ 26-graphs/graph_add_edge.js
@@ -0,0 +1,23 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+}
+
+let g = new Graph();
+g.addVertex("철수");
+g.addVertex("영희");
+g.addVertex("맹구");
+
+g.addEdge("철수", "맹구");
+
+console.log(g.adjacencyList);
+
+// 일단은 양방향 그래프.
