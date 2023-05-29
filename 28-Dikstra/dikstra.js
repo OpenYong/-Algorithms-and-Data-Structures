ff5c47c811b279aeadabef26aa0212a18c6ce060
OpenYong <fkszm3@gmail.com>
2023-05-29 15:17:51 +0900

    Dikstra 구현 복습

--- 28-Dikstra/dikstra.js
+++ 28-Dikstra/dikstra.js
@@ -29,25 +29,27 @@ class WeightedGraph {
     const nodes = new PriorityQueue();
     const distances = {};
     const previous = {};
-    let path = []; //to return at end
+    let path = []; // 거쳐온 경로
     let smallest;
-    //build up initial state
+
+    // 초기 상태를 만들어 준다.
+    // 시작 지점을 제외하고 모두 무한대로 설정함.
     for (let vertex in this.adjacencyList) {
       if (vertex === start) {
         distances[vertex] = 0;
         nodes.enqueue(vertex, 0);
       } else {
         distances[vertex] = Infinity;
-        nodes.enqueue(vertex, Infinity);
+        nodes.enqueue(vertex, Infinity); // 우선순위 큐에 넣어 준다.
       }
       previous[vertex] = null;
     }
-    // as long as there is something to visit
+
+    // 각 정점을 방문할 동안 실행되는 코드
     while (nodes.values.length) {
       smallest = nodes.dequeue().val;
       if (smallest === finish) {
-        //WE ARE DONE
-        //BUILD UP PATH TO RETURN AT END
+        // 끝
         while (previous[smallest]) {
           path.push(smallest);
           smallest = previous[smallest];
@@ -56,17 +58,16 @@ class WeightedGraph {
       }
       if (smallest || distances[smallest] !== Infinity) {
         for (let neighbor in this.adjacencyList[smallest]) {
-          //find neighboring node
+          // 이웃 노드 찾기
           let nextNode = this.adjacencyList[smallest][neighbor];
-          //calculate new distance to neighboring node
+          // 이웃 노드까지의 거리 계산
           let candidate = distances[smallest] + nextNode.weight;
           let nextNeighbor = nextNode.node;
           if (candidate < distances[nextNeighbor]) {
-            //updating new smallest distance to neighbor
+            // 만약 최소 거리가 된다면 distances 객체를 업데이트 해준다.
             distances[nextNeighbor] = candidate;
-            //updating previous - How we got to neighbor
+            // 해당 정점까지 갔을 때, 방문한 직전 정점을 저장해준다.
             previous[nextNeighbor] = smallest;
-            //enqueue in priority queue with new priority
             nodes.enqueue(nextNeighbor, candidate);
           }
         }
@@ -94,5 +95,6 @@ graph.addEdge("D", "F", 1);
 graph.addEdge("E", "F", 1);
 
 graph.Dijkstra("A", "E");
+console.log(graph.Dijkstra("A", "E"));
 
 // ["A", "C", "D", "F", "E"]
